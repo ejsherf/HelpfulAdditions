e5a89df96af94af91e39956a799f8626f4592840
EJ <37851212+ejsherf@users.noreply.github.com>
2021-08-21 22:04:06 -0400

    Deleted some commands causing errors, will fix in a later release.

--- src/main/java/me/ej/HelpfulAdditions/EJessentials.java
+++ src/main/java/me/ej/HelpfulAdditions/EJessentials.java
@@ -2,7 +2,6 @@ package me.ej.HelpfulAdditions;
 
 import me.ej.HelpfulAdditions.commands.*;
 import me.ej.HelpfulAdditions.events.*;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EJessentials extends JavaPlugin {
@@ -10,16 +9,13 @@ public final class EJessentials extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         System.out.println("HelpfulAdditions by EJ is starting! Hey there friend!");
-        getCommand("server").setExecutor(new ServerCMD());
         getCommand("plugininfo").setExecutor(new PluginInfo());
         getCommand("playerUUID").setExecutor(new UUIDcmd());
         getServer().getPluginManager().registerEvents(new JoinBed(),this);
         getServer().getPluginManager().registerEvents(new JoinMsg(),this);
         getServer().getPluginManager().registerEvents(new LeaveBed(),this);
         getServer().getPluginManager().registerEvents(new LeaveMsg(),this);
-        getCommand("renderdistance").setExecutor(new RenderDistance());
         getCommand("online").setExecutor(new OnlinePlayers());
-        getCommand("serverversion").setExecutor(new ServerVersionCMD());
         getServer().getPluginManager().registerEvents(new ItemDrop(),this);
         getServer().getPluginManager().registerEvents(new ItemPickup(),this);
     }
--- src/main/java/me/ej/HelpfulAdditions/commands/PluginInfo.java
+++ src/main/java/me/ej/HelpfulAdditions/commands/PluginInfo.java
@@ -9,9 +9,9 @@ import org.bukkit.entity.Player;
 
 public class PluginInfo implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player || sender instanceof ConsoleCommandSender) {
-            if (sender.hasPermission("helpfuladdititons.plugininfo")) {
-                sender.sendMessage(ChatColor.GOLD + "HelpfulAdditions is a plugin that has a few nice things to have on your server. Developer: EJ. ");
+        if (sender instanceof Player || sender instanceof ConsoleCommandSender){
+            if (sender.hasPermission("helpfuladdititons.plugininfo")){
+                sender.sendMessage(ChatColor.GOLD + "HelpfulAdditions is a plugin that has a few nice things to have on your server. Developer: EJ.");
             } else {
                 sender.sendMessage(ChatColor.RED+"Looks like you don't have the permission to run this. This is a mistake. Contact your server administrator.");
             }
